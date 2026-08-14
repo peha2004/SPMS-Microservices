8f8eef2af90dfbaabcf1e92d5957837c94e8755d
Ranuthi Pehansa <ranuthi2004@gmail.com>
2026-08-15 00:43:54 +0530

    Add payment API response utility

---
+++ payment-service/src/main/java/lk/ijse/paymentservice/util/ApiResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.paymentservice.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+}
